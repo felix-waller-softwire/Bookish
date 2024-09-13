054ef80b62b1452eea8bec6195d6057be1992401
Felix Waller <felix.waller@softwire.com>
2024-09-13 16:28:47 +0100

    Create book

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,4 +6,13 @@ class BooksController < ApplicationController
   def show
     @book = Book.find(params[:id])
   end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.create(params.require(:book).permit(:title, :author, :isbn))
+    redirect_to book_url(@book)
+  end
 end
